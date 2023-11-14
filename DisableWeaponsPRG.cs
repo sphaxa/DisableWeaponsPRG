78de1e3c49089a831ce5fed9e6ee52598d3a8da3
sphhax <zskaistis2@gmail.com>
2023-11-14 10:58:22 -0600

    Add project files.

---
+++ DisableWeaponsPRG.cs
@@ -0,0 +1,123 @@
+﻿using CounterStrikeSharp.API.Core;
+using System.Text.Json;
+
+namespace DisableWeaponsPRG;
+public class DisableWeaponsPRG : BasePlugin
+{
+    public override string ModuleAuthor => "sphaxa";
+    public override string ModuleName => "DisableWeapons for PRG";
+    public override string ModuleVersion => "v1.0.0";
+
+    private List<Config>? _config = new();
+
+    public override void Load(bool hotReload)
+    {
+        var configPath = Path.Join(ModuleDirectory, "DisableWeaponsConfig.json");
+        _config = !File.Exists(configPath) ? CreateConfig(configPath) : JsonSerializer.Deserialize<List<Config>>(File.ReadAllText(configPath));
+
+        RegisterEventHandler<EventItemPickup>(OnItemPickup, HookMode.Post);
+        RegisterEventHandler<EventItemPurchase>(OnItemPurchase, HookMode.Post);
+
+        RegisterListener<Listeners.OnEntitySpawned>(entity =>
+        {
+            if (entity == null) return;
+            if (entity.Entity == null) return;
+            if (!entity.DesignerName.StartsWith("weapon_")) return;
+
+            if (!IsBlocked(entity.DesignerName)) return;
+
+            entity.Remove();
+        });
+    }
+
+    private HookResult OnItemPickup(EventItemPickup @event, GameEventInfo info)
+    {
+        if (!@event.Userid.IsValid)
+        {
+            return HookResult.Continue;
+        }
+
+        CCSPlayerController player = @event.Userid;
+
+        if (player.Connected != PlayerConnectedState.PlayerConnected)
+        {
+            return HookResult.Continue;
+        }
+
+        if (!player.PlayerPawn.IsValid)
+        {
+            return HookResult.Continue;
+        }
+        var wep = MetaData.Weapons.FirstOrDefault(w => w.DefIndex == @event.Defindex);
+        if (wep == null) return HookResult.Continue;
+
+        if (!IsBlocked(wep.WeaponName)) return HookResult.Continue;
+
+        if (player.PlayerPawn.Value.WeaponServices == null) return HookResult.Continue;
+        foreach (var invwep in player.PlayerPawn.Value.WeaponServices.MyWeapons)
+        {
+            if (invwep.Value.AttributeManager.Item.ItemDefinitionIndex != @event.Defindex) continue;
+            invwep.Value.Remove();
+            NativeAPI.IssueClientCommand((int)@event.Userid.EntityIndex!.Value.Value - 1, "lastinv");
+        }
+
+        return HookResult.Continue;
+    }
+
+    private HookResult OnItemPurchase(EventItemPurchase @event, GameEventInfo info)
+    {
+        if (!@event.Userid.IsValid)
+        {
+            return HookResult.Continue;
+        }
+
+        CCSPlayerController player = @event.Userid;
+
+        if (player.Connected != PlayerConnectedState.PlayerConnected)
+        {
+            return HookResult.Continue;
+        }
+
+        if (!player.PlayerPawn.IsValid)
+        {
+            return HookResult.Continue;
+        }
+
+
+        var wep = MetaData.Weapons.FirstOrDefault(w => w.WeaponName == @event.Weapon);
+        if (wep == null) return HookResult.Continue;
+
+        if (!IsBlocked(wep.WeaponName)) return HookResult.Continue;
+
+        @event.Userid.InGameMoneyServices!.Account += wep.Price;
+
+        return HookResult.Continue;
+    }
+
+    private static List<Config> CreateConfig(string configPath)
+    {
+        var data = new List<Config> { new() { Name = "weapon_awp"} };
+        File.WriteAllText(configPath, JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true, Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping }));
+        return data;
+    }
+
+    private bool IsBlocked(string DesignerName)
+    {
+        var wep = _config?.FirstOrDefault(k => k.Name == DesignerName);
+        if (wep == null) return false;
+        return true;
+    }
+}
+
+public class WeaponMeta
+{
+    public string? WeaponName { get; init; }
+    public string? Name { get; init; }
+    public int Price { get; init; }
+    public long DefIndex { get; init; }
+}
+
+public class Config
+{
+    public string? Name { get; set; }
+}
---
+++ MetaData.cs
@@ -0,0 +1,299 @@
+﻿using CounterStrikeSharp.API.Modules.Entities.Constants;
+
+namespace DisableWeaponsPRG
+{
+    public static class MetaData
+    {
+        // THANKS TO QUAKE1011 FOR THE WEAPON META DATA
+        public static List<WeaponMeta> Weapons = new List<WeaponMeta>
+        {
+            new()
+            {
+                DefIndex = (long)ItemDefinition.M4A4,
+                Name = "M4A1",
+                WeaponName = "weapon_m4a1",
+                Price = 3100,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.M4A1_S,
+                Name = "M4A1-S",
+                WeaponName = "weapon_m4a1_silencer",
+                Price = 2900,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.FAMAS,
+                Name = "Famas",
+                WeaponName = "weapon_famas",
+                Price = 2050,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.AUG,
+                Name = "AUG",
+                WeaponName = "weapon_aug",
+                Price = 3300,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.AK_47,
+                Name = "AK-47",
+                WeaponName = "weapon_ak47",
+                Price = 2700,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.GALIL_AR,
+                Name = "Galil",
+                WeaponName = "weapon_galilar",
+                Price = 1800,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.SG_553,
+                Name = "Sg553",
+                WeaponName = "weapon_sg556",
+                Price = 3000,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.SCAR_20,
+                Name = "Scar-20",
+                WeaponName = "weapon_scar20",
+                Price = 5000,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.AWP,
+                Name = "AWP",
+                WeaponName = "weapon_awp",
+                Price = 4750,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.SSG_08,
+                Name = "SSG08",
+                WeaponName = "weapon_ssg08",
+                Price = 1700,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.G3SG1,
+                Name = "G3SG1",
+                WeaponName = "weapon_g3sg1",
+                Price = 5000,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.MP9,
+                Name = "MP9",
+                WeaponName = "weapon_mp9",
+                Price = 1250,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.MP7,
+                Name = "MP7",
+                WeaponName = "weapon_mp7",
+                Price = 1500,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.MP5_SD,
+                Name = "MP5-SD",
+                WeaponName = "weapon_mp5sd",
+                Price = 1500,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.UMP_45,
+                Name = "UMP-45",
+                WeaponName = "weapon_ump45",
+                Price = 1200,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.P90,
+                Name = "P-90",
+                WeaponName = "weapon_p90",
+                Price = 2350,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.PP_BIZON,
+                Name = "PP-19 Bizon",
+                WeaponName = "weapon_bizon",
+                Price = 1400,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.MAC_10,
+                Name = "MAC-10",
+                WeaponName = "weapon_mac10",
+                Price = 1050,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.USP_S,
+                Name = "USP-S",
+                WeaponName = "weapon_usp_silencer",
+                Price = 200,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.P2000,
+                Name = "P2000",
+                WeaponName = "weapon_hkp2000",
+                Price = 200,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.GLOCK_18,
+                Name = "Glock-18",
+                WeaponName = "weapon_glock",
+                Price = 200,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.DUAL_BERETTAS,
+                Name = "Dual berettas",
+                WeaponName = "weapon_elite",
+                Price = 300,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.P250,
+                Name = "P250",
+                WeaponName = "weapon_p250",
+                Price = 300,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.FIVE_SEVEN,
+                Name = "Five-SeveN",
+                WeaponName = "weapon_fiveseven",
+                Price = 500,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.CZ75_AUTO,
+                Name = "CZ75-Auto",
+                WeaponName = "weapon_cz75a",
+                Price = 500,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.TEC_9,
+                Name = "Tec-9",
+                WeaponName = "weapon_tec9",
+                Price = 500,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.R8_REVOLVER,
+                Name = "Revolver R8",
+                WeaponName = "weapon_revolver",
+                Price = 600,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.DESERT_EAGLE,
+                Name = "Desert Eagle",
+                WeaponName = "weapon_deagle",
+                Price = 700,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.NOVA,
+                Name = "Nova",
+                WeaponName = "weapon_nova",
+                Price = 1050,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.XM1014,
+                Name = "XM1014",
+                WeaponName = "weapon_xm1014",
+                Price = 2000,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.MAG_7,
+                Name = "MAG-7",
+                WeaponName = "weapon_mag7",
+                Price = 1300,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.SAWED_OFF,
+                Name = "Sawed-off",
+                WeaponName = "weapon_sawedoff",
+                Price = 1100,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.M249,
+                Name = "M429",
+                WeaponName = "weapon_m249",
+                Price = 5200,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.NEGEV,
+                Name = "Negev",
+                WeaponName = "weapon_negev",
+                Price = 1700,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.ZEUS_X27,
+                Name = "Zeus x27",
+                WeaponName = "weapon_taser",
+                Price = 200,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.HIGH_EXPLOSIVE_GRENADE,
+                Name = "High Explosive Grenade",
+                WeaponName = "weapon_hegrenade",
+                Price = 300,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.MOLOTOV,
+                Name = "Molotov",
+                WeaponName = "weapon_molotov",
+                Price = 400,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.INCENDIARY_GRENADE,
+                Name = "Incendiary Grenade",
+                WeaponName = "weapon_incgrenade",
+                Price = 600,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.SMOKE_GRENADE,
+                Name = "Smoke Grenade",
+                WeaponName = "weapon_smokegrenade",
+                Price = 300,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.FLASHBANG,
+                Name = "Flashbang",
+                WeaponName = "weapon_flashbang",
+                Price = 200,
+            },
+            new()
+            {
+                DefIndex = (long)ItemDefinition.DECOY_GRENADE,
+                Name = "Decoy Grenade",
+                WeaponName = "weapon_decoy",
+                Price = 50
+            }
+        };
+    }
+}
